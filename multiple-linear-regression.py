6a17fc4235f7cb88768ff9b5e296aa48dd645784
pfigithub <pampam7072@gmail.com>
2024-02-27 16:57:06 +0330

    predict

--- multiple-linear-regression.py
+++ multiple-linear-regression.py
@@ -26,3 +26,13 @@ y = np.asanyarray(train[['CO2EMISSIONS']])
 regr.fit (x, y)
 # The coefficients
 print ('Coefficients: ', regr.coef_)
+
+# prediction
+y_hat= regr.predict(test[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_COMB']])
+x = np.asanyarray(test[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_COMB']])
+y = np.asanyarray(test[['CO2EMISSIONS']])
+print("Residual sum of squares: %.2f"
+      % np.mean((y_hat - y) ** 2))
+
+# Explained variance score: 1 is perfect prediction
+print('Variance score: %.2f' % regr.score(x, y))
