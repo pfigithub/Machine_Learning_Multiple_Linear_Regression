de833fde2517150cffe5840d14f2b6786773106a
pfigithub <pampam7072@gmail.com>
2024-02-27 16:52:55 +0330

    read

--- multiple-linear-regression.py
+++ multiple-linear-regression.py
@@ -1,3 +1,9 @@
 # importing packages
 import pandas as pd
 import numpy as np
+
+# reading data(our data is fuelconsumption)
+df = pd.read_csv("yourdata.csv")
+
+# take a look at the dataset
+df.head()
