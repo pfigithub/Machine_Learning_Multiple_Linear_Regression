5784b2f2cf5a9f634244ff109c655974c36e641c
pfigithub <pampam7072@gmail.com>
2024-02-27 16:53:42 +0330

    select

--- multiple-linear-regression.py
+++ multiple-linear-regression.py
@@ -7,3 +7,7 @@ df = pd.read_csv("yourdata.csv")
 
 # take a look at the dataset
 df.head()
+
+# selecting features
+cdf = df[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_CITY','FUELCONSUMPTION_HWY','FUELCONSUMPTION_COMB','CO2EMISSIONS']]
+cdf.head(9)
